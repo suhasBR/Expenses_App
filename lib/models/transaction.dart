4e2b9cdddd5db98ec86c3cf9fac3ace810fea1f9
FlutLab.io <>
2021-08-03 12:09:50 +0000

    Initial commit

---
+++ lib/models/transaction.dart
@@ -0,0 +1,15 @@
+//to get @required we import foundation.dart
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  @required
+  final String id;
+  @required
+  final String title;
+  @required
+  final double amount;
+  @required
+  final DateTime date;
+
+  Transaction({this.id, this.title, this.amount, this.date});
+}
